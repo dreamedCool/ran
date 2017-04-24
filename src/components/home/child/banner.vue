a1a6e90e59d8e02c73782d8a83f553249422ec3a
liangmengyuan <liangmengyuan@wecash.net>
2017-04-24 17:14:52 +0800

    首页

--- src/components/home/child/banner.vue
+++ src/components/home/child/banner.vue
@@ -1,25 +1,32 @@
 <style src='../less/banner.less' lang='less' scoped></style>
 <template>
   <div class="wrapper">
-    33
+    <div class="banner" v-for='item in list'>
+      <img :src="item.url"/>
+    </div>
   </div>
 </template>
+<style scoped>
+</style>
+
 <script>
   export default {
     name: 'banner',
     data () {
-      return {}
+      return {
+        list: [
+          {
+            url: 'static/banner.jpg'
+          }
+        ]
+      }
     },
     mounted () {
       this.getAjax()
     },
     methods: {
       getAjax () {
-        let params = {
-          Cookie: "rpc-key='ovvTH9HICcdb6K9I'",
-          Authorization: 'ZjBkODBiNzAxY2MyNDE0MWEzMzVkNmE4YjBhYzU0NDI6MTg2MTI3OTIxMTA6MToxNzIyOjFjYmViM2Y0MjEyM2ZhOGVkOGRjMzc3OWYzOTJmYzU2'
-        }
-        this.$http.post('http://dev.artful.com.cn/api/artful/events/booking', params).then((res) => {
+        this.$http.get('/lw/collections/094c9ec162967dc3c4c2').then((res) => {
           console.log(res)
         })
       }
--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -19,22 +19,46 @@
           </div>
         </template>
         <div class="life">
-          <div class="life-title">
+          <div class="title">
             <h3>燃活动</h3>
             <div class="more">
               <span>更多</span>
               <i class="icon i-more"></i>
             </div>
           </div>
-          <div class="life-detail-wrapper">
-            <span class="life-detail-name">燃画布</span>
-            <span class="life-detail-english">MYCANVAS</span>
+          <div class="life-detail-wrapper" 
+          v-for='item in life' :style="{backgroundImage: 'url('+item.url+')'}">
+            <span class="life-detail-name">{{item.name}}</span>
+            <span class="life-detail-english">{{item.englishName}}</span>
             <i class="icon i-store life-detail-store"></i>
-            <span class="pic-name">油画.回眸</span>
-            <em class="pic-price">￥299</em>
+            <span class="pic-name">{{item.picName}}</span>
+            <em class="pic-price">￥{{item.price}}</em>
             <div class="pic-date">
               <i class="icon i-date"></i>
-              <time>2017-02-02</time>
+              <time>{{item.date}}</time>
+            </div>
+            <div class="pic-address">
+              <i class="icon i-location"></i>
+              <span>{{item.address}}</span>
+            </div>
+          </div>
+        </div>
+        <div class="artist">
+          <div class="title">
+            <h3>艺术家</h3>
+            <div class="more">
+              <span>更多</span>
+              <i class="icon i-more"></i>
+            </div>
+          </div>
+          <div class="focus">
+            <div class='people-info'>
+              <div class="portrait"></div>
+              <span>陈文夏</span>
+            </div>
+            <div class='people-info'>
+              <div class="portrait"></div>
+              <span>陈文夏</span>
             </div>
           </div>
         </div>
@@ -45,7 +69,30 @@
   export default {
     name: 'test',
     data () {
-      return {}
+      return {
+        life: [
+          {
+            url: 'static/life.jpg',
+            name: '燃画布',
+            englishName: 'MYCANVAS',
+            storeStatus: false,
+            picName: '油画回眸',
+            price: 299,
+            date: '2017.02.02',
+            address: '北京三里屯'
+          },
+          {
+            url: 'static/life2.jpg',
+            name: '燃活动',
+            englishName: '燃show',
+            storeStatus: true,
+            picName: '当代艺术三蕴',
+            price: 322,
+            date: '2017.02.02',
+            address: '北京三里屯'
+          }
+        ]
+      }
     },
     components: {
       banner

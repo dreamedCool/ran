538bfb144bd8ca1818db37ea080496fab460a60d
liangmengyuan <liangmengyuan@wecash.net>
2017-05-03 15:22:58 +0800

    update

--- src/components/personal/pdata.vue
+++ src/components/personal/pdata.vue
@@ -4,7 +4,12 @@
     <div class="content">
       <div class="upload-wrapper">
         <div class="upload">
-          <div class='user-photo'></div>
+          <div class='user-photo'>
+            <img :src='dataUrl'/>
+            <label for="upload-file">
+              <input type="file" id="upload-file" v-on:change="upload"/>
+            </label>
+          </div>
           <div class='take-photo'>
             <i class="icon i-photo"></i>
           </div>
@@ -13,38 +18,71 @@
 
       <div class="name">
         <label>昵称</label>
-        <span class="font-color">专属用户名，会令你与众不同</span>
-        <i class="icon i-more"></i>
+        <div>
+          <span class="font-color">专属用户名，会令你与众不同</span>
+          <i class="icon i-more"></i>
+        </div>
       </div>
 
       <div class="sex">
         <label>性别</label>
-        <div>
+        <div class="sex-icon">
           <span class="user-sex"></span> <i class="icon i-girl"></i>
-          <span type="radio"></span><i class="icon i-boy"></i>
+          <span class="user-sex"></span><i class="icon i-boy"></i>
         </div>
       </div>
 
       <div class="address">
         <label>地址</label>
-        <span class="font-color">选择您所在城市</span>
-        <i class="icon i-more"></i>
+        <div>
+          <span class="font-color">选择您所在城市</span>
+          <i class="icon i-more"></i>
+        </div>
       </div>
 
       <div class="birth">
         <label>生日</label>
-        <span class="font-color">1980-01-01</span>
-        <i class="icon i-more"></i>
+        <div>
+          <span class="font-color">1980-01-01</span>
+          <i class="icon i-more"></i>
+        </div>
       </div>
 
       <div class="sign">
         <label>个性签名</label>
-        <span class="font-color">编辑个性签名</span>
-        <i class="icon i-more"></i>
+        <div>
+          <span class="font-color">编辑个性签名</span>
+          <i class="icon i-more"></i>
+        </div>
+      </div>
+      <div class="btn">
+        <div class="submit">保存</div>
       </div>
     </div>
   </div>
 </template>
 <script>
-
+  export default {
+    name: 'userData',
+    data () {
+      return {
+        dataUrl: ''
+      }
+    },
+    methods: {
+      upload (e) {
+        let self = this
+        let fileType = ['image/jpeg', 'image/jpg', 'image/png']
+        let file = e.target.files[0]
+        console.log(file)
+        if (file && file.type.indexOf(fileType)) {
+          let reader = new FileReader()
+          reader.readAsDataURL(file)
+          reader.onloadend = function () {
+            self.dataUrl = this.result
+          }
+        }
+      }
+    }
+  }
 </script>

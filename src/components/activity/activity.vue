0c7f4bb9b989c7ddb5b4b6a7467572fc2c39e0c5
motif <dinghuihua@evermarker.net>
2017-05-08 15:03:15 +0800

    add activity page data

--- src/components/activity/activity.vue
+++ src/components/activity/activity.vue
@@ -4,38 +4,38 @@
     <Banner></Banner>
     <div class="nav">
       <div class="nav-item">
-        <div class="hot active"></div>
+        <div class="hot" :class="{active:isHotMenu}" @click="toggleMenu(true)"></div>
       </div>
       <div class="nav-item">
-        <div class="history"></div>
+        <div class="history" :class="{active:!isHotMenu}" @click="toggleMenu(false)"></div>
       </div>
     </div>
-    <div class="list-wrapper">
-      <div class="list-item">
+    <div class="list-wrapper" v-show="isHotMenu">
+      <div class="list-item" v-for="item in hotList">
         <div class="top">
           <div class="timer-wrapper">
             <span class="date">2.19</span>
             <span class="time">周三10:00</span>
           </div>
-          <div class="button-wrapper">
-            <div class="btn-remind">{{btnText}}</div>
+          <div class="button-wrapper" v-show="item.ticketCount===0">
+            <div class="btn-remind">空位提醒</div>
           </div>
         </div>
         <div class="main-img">
-          <img src="static/activityImg.png">
+          <img :src="item.photo.url">
         </div>
         <div class="info-wrapper">
           <div class="info-top">
             <div class="main">
               <div class="avator">
-                <img src="static/portait.png">
+                <img :src="item.teacher.avatar">
               </div>
               <div class="tit-wrapper">
                 <div class="title">
-                  <span>油画.回眸</span>
-                  <span class="tip">已报满</span>
+                  <span class="topic">{{item.topic}}</span>
+                  <span class="tip" :class="{full:item.ticketCount === 0}">{{item.ticketCount>0?'报名中':'已报满'}}</span>
                 </div>
-                <span class="teacher">指导: 陈文夏</span>
+                <span class="teacher">指导: {{item.teacher.name}}</span>
               </div>
             </div>
             <div class="store">
@@ -45,27 +45,62 @@
           <div class="info-bottom">
             <div class="addr-msg">
               <i class="icon i-location"></i>
-              <span class="address">北京三里屯朝外soho</span>
+              <span class="address">{{item.location.name}}</span>
               <span class="distance">1.3km</span>
             </div>
-            <div class="price">￥299</div>
+            <div class="price">￥{{item.price}}</div>
           </div>
         </div>
       </div>
+    </div>
+    <div style="height: 500px; background: red;" v-show="!isHotMenu">
     </div>
   </div>
 </template>
 <script>
   import Banner from './child/banner'
+  let ERR_CODE = 0
   export default {
     name: 'activity',
     data () {
       return {
-        btnText: '空位提醒'
+        isHotMenu: true,
+        hotList: [],
+        historyList: []
       }
     },
     components: {
       Banner
+    },
+    computed: {
+      tipText () {
+        return this.ticketCount > 0 ? '报名中' : '已报满'
+      }
+    },
+    mounted () {
+      this.getHotList()
+      this.getHistoryList()
+    },
+    methods: {
+      toggleMenu (param) {
+        this.isHotMenu = param
+      },
+      getHotList () {
+        this.$http.get('/lw/events/recommended').then(res => {
+          let resData = res.body
+          if (resData.errorCode === ERR_CODE) {
+            this.hotList = resData.data
+          }
+        })
+      },
+      getHistoryList () {
+        this.$http.get('/lw/events/history').then(res => {
+          let resData = res.body
+          if (resData.errorCode === ERR_CODE) {
+            this.historyList = resData.data.list
+          }
+        })
+      }
     }
   }
 </script>

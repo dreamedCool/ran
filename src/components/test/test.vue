1c5e1abb2a9547ddfdfee891213afd0d9011f36f
liangmengyuan <liangmengyuan@wecash.net>
2017-05-11 18:57:39 +0800

    update

--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -1,64 +1,41 @@
 <style lang="less" src="./less/test.less" scoped></style>
-<style lang="less" scoped>
-
-</style>
 <template>
-  <div class="bg">
-    <img src="/static/test/pointer.png" class="pointer" @click="turn">
-    <img src="/static/test/turntable.png" alt="turntable" class="turntable">
+  <div>
+    <Mytitle></Mytitle>
+    <div class="bg">
+      <img src="/static/test/pointer.png" class="pointer">
+      <img src="/static/test/turntable.png" alt="turntable" class="turntable">
+    </div>
+    <div class="button" @click="turn">点击</div>
   </div>
 </template>
 <script>
+  import Mytitle from './title.vue'
   export default {
     data () {
       return {
-        cat: 51.4,
-        num: 0,
+        cat: 51.4, // 每格的度数
+        deg: 0, // 转动的初始度数,
+        olddeg: 0,
         offOn: true
       }
     },
+    components: {Mytitle},
     methods: {
       turn () {
-        let oTurntable = document.getElementsByTagName('img')[1]
         if (this.offOn) {
-          oTurntable.style.transform = 'rotate(0deg)'
           this.offOn = !this.offOn
           this.ratating()
         }
       },
       ratating () {
+        this.offOn = !this.offOn
         let oTurntable = document.getElementsByTagName('img')[1]
-        let timer = null
-        let rdm = 0
-        clearInterval(timer)
-        timer = setInterval(() => {
-          if (Math.floor(rdm / 360) < 3) {
-            rdm = Math.floor(Math.random() * 3600)
-          } else {
-            oTurntable.style.transform = 'rotate(' + rdm + 'deg)'
-            clearInterval(timer)
-            setTimeout(() => {
-              this.offOn = !this.offOn
-              this.num = rdm % 360
-              console.log(this.num)
-              if (this.num <= this.cat * 1) {
-                alert('4999')
-              } else if (this.num <= this.cat * 2) {
-                alert('条50元')
-              } else if (this.num <= this.cat * 3) {
-                alert('10元')
-              } else if (this.num <= this.cat * 4) {
-                alert('5元')
-              } else if (this.num <= this.cat * 5) {
-                alert('免息服务')
-              } else if (this.num <= this.cat * 6) {
-                alert('提交白金')
-              } else {
-                alert('未中奖')
-              }
-            }, 4000)
-          }
-        }, 30)
+        this.deg = Math.floor(Math.random() * 3600)
+        oTurntable.style.transform = 'rotate(' + this.deg + 'deg)'
+        setTimeout(() => {
+          alert(1)
+        }, 4000)
       }
     }
   }
---
+++ src/components/test/title.vue
@@ -0,0 +1,63 @@
+<style lang="less" scoped>
+ul,li{margin:0; padding:0; }
+li{ list-style:none; }
+.container{
+  width: 10rem;
+  height: 88/75rem;
+  background:#efefef;
+  margin-bottom: 20/75rem;
+  overflow: hidden;
+ }
+.container .li-box{
+    width: 10rem;
+   height:88/75rem; 
+   overflow:hidden;
+}
+.container ul{ 
+  position:relative;
+  transition: all 1s; 
+}
+.container li{
+  height: 88/75rem; 
+  line-height:88/75rem; 
+  color:#666; 
+  }
+</style>
+<template>
+  <div class="container">
+    <div class="li-box">
+      <ul ref='wrapper'>
+        <li v-for="title in titles" v-if='state'>{{title}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        titles: ['飞鲨', '勇', '曾驾歼-8'],
+        num: 0,
+        state: true,
+        timer: null
+      }
+    },
+    mounted () {
+      this.initScroll()
+    },
+    methods: {
+      initScroll () {
+        this.timer = setInterval(() => {
+          this.scroll()
+        }, 1000)
+      },
+      scroll () {
+        ++this.num
+        if (this.num > this.titles.length) {
+          this.num = 0
+        }
+        this.$refs.wrapper.style.transform = 'translateY(' + (-88 * this.num) / 75 + 'rem)'
+      }
+    }
+  }
+</script>
